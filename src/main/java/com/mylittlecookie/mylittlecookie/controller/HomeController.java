654a99386397ad883e52b35a301ec96b5e3e70fd
EunhyeSong <dmsgp8292@gmail.com>
2022-04-08 01:40:59 +0900

    4/7

--- src/main/java/com/mylittlecookie/mylittlecookie/controller/HomeController.java
+++ src/main/java/com/mylittlecookie/mylittlecookie/controller/HomeController.java
@@ -2,14 +2,13 @@ package com.mylittlecookie.mylittlecookie.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home (){
-        return "home";
+    public String main (){
+        return "main";
     }
 
 }
